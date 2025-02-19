f8b12d263bc263561d2591cab67c33951d507154
Ed Hunter <ep.hunter23@gmail.com>
2025-02-19 08:43:22 +0800

    feat: add error message logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import "./style.css";
+console.log("test");
 
-let email = document.querySelector("input#email");
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const zipcode = document.getElementById("zipcode");
+const password = document.getElementById("password");
+const passwordconfirm = document.getElementById("passwordconfirm");
+
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (email.value === "" || email.value == null) {
+    messages.push("Email is required");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
+  }
+});
 
 email.addEventListener("focusout", () => {
   if (email.validity.typeMismatch) {
