881d9373eeb490ae039fdba5668ee98bf2dec02a
Ed Hunter <ep.hunter23@gmail.com>
2025-03-31 07:42:22 +0800

    feat: constraint validation made dynamic

--- src/constraints.js
+++ src/constraints.js
@@ -1,10 +1,40 @@
 class Constraints {
   constructor(type, minLength, maxLength, pattern, required) {
     this.type = type;
-    [this.minLength, this.maxLength] = [minLength, maxLength];
+    this.minLength = minLength;
+    this.maxLength = maxLength;
     this.pattern = pattern;
     this.required = required;
   }
 }
 
+//constraint function
+function constraintsBool(inputData, constraints) {
+  const { type, minLength, maxLength, pattern, required } = constraints;
+
+  inputData.setCustomValidity("");
+
+  if (inputData.value.length < minLength) {
+    inputData.setCustomValidity(
+      `This ${type} is too short, please input a longer ${type}. Oink!`,
+    );
+  }
+  if (inputData.value.length > maxLength) {
+    inputData.setCustomValidity(
+      `This ${type} is too long please input a shorter ${type}. Oink!`,
+    );
+  }
+  if (!new RegExp(pattern).test(inputData.value)) {
+    inputData.setCustomValidity(
+      `This ${type} is invalid, please input a ${type}. Oink!`,
+    );
+  }
+  if (required && !inputData.value) {
+    inputData.setCustomValidity(`A ${type} is absolutely needed. Oink!`);
+  }
+
+  // if () {
+  // }
+}
 export default Constraints;
+export { constraintsBool };
--- src/index.js
+++ src/index.js
@@ -1,50 +1,18 @@
 import "./style.css";
-import Constraints from "./constraints";
 import addInputListeners from "./addListeners";
 //e.target grabs element
 //type, minLength, maxLength, pattern, required
 addInputListeners();
 
-const emailConstraints = new Constraints(
-  "Email",
-  5,
-  30,
-  "[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}",
-  true,
-);
-
-const countyConstraints = new Constraints(
-  "Country",
-  4,
-  20,
-  "[a-zA-Z]{2,}",
-  true,
-);
-
-const zipcodeConstraints = new Constraints("zip code", 4, 4, "[0-9]+", true);
-
-const passwordConstraints = new Constraints(
-  "password",
-  7,
-  25,
-  "[a-zA-Z]{2,}",
-  true,
-);
-
-const pwConfirmConstraints = new Constraints(
-  "Email",
-  7,
-  25,
-  "[a-zA-Z]{2,}",
-  true,
-);
-
 // run these through input.setCustomValidity()
-
 // console.log(`${e.target.checkValidity()} `);
-
 // object{type, minLength, maxLength, pattern, required}
 // skip, rangeUnderflow, rangeOverflow, patternMismatch,valueMissing
 ////passwords need to equal each other
 
 //onsubmit message
+
+//TODO
+//[]need to hook up these two...
+//// object{type, minLength, maxLength, pattern, required}
+//// skip, rangeUnderflow, rangeOverflow, patternMismatch,valueMissing
